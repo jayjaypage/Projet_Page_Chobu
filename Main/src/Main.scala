160234c2513c722b4b5f94d3dce67720b28a51df
Vchobu <vadym.chobu@gmail.com>
2023-12-21 11:54:22 +0100

    Base of the game

--- Main/src/Main.scala
+++ Main/src/Main.scala
@@ -1,5 +1,14 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
-  }
+import hevs.graphics.FunGraphics
+
+import java.awt.color
+import scala.collection.mutable.ArrayBuffer
+class Board(x: Int, y: Int) {
+  var length : Int = 3
+  var snake : ArrayBuffer[(Int, Int)] = ArrayBuffer((x/2+1,y/2),(x/2,y/2),(x/2-1,y/2))
+  var apple : ArrayBuffer[(Int, Int)] = ArrayBuffer((0,0))
+  // create a function that will create borders(-2) depending on the size
+}
+
+object SnakeGame extends App {
+val field : Board = new Board(10, 10)
 }
